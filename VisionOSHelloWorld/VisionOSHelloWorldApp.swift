7e4f8121932c002694fa61b2878dc74bfc23987a
aaronsiim <web3aaron@gmail.com>
2023-07-03 13:32:51 -0400

    Initial Commit

---
+++ VisionOSHelloWorld/VisionOSHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSHelloWorldApp.swift
+//  VisionOSHelloWorld
+//
+//  Created by Aaron Sim on 2023-07-03.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
